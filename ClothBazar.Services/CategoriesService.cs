0aa4ee89d08887189ffe65de287a2d66ddf01b4e
Krishna Rana <info2krishnarana@gmail.com>
2019-04-02 08:07:22 +0530

    create/update/delete catetory completed

--- ClothBazar.Services/CategoriesService.cs
+++ ClothBazar.Services/CategoriesService.cs
@@ -2,6 +2,7 @@
 using ClothBazar.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,46 @@ namespace ClothBazar.Services
 {
     public class CategoriesService
     {
+        public List<Category> GetCategories()
+        {
+            using (CBContext context = new CBContext())
+            {
+              return  context.Categories.ToList();
+            }
+        }
+
         public void SaveCategory(Category category)
         {
             using (CBContext context=new CBContext())
             {
                 context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public Category GetCategory(int id)
+        {
+            using (CBContext context = new CBContext())
+            {
+               return context.Categories.Find(id);
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (CBContext context = new CBContext())
+            {
+                context.Entry(category).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int id)
+        {
+            using (CBContext context = new CBContext())
+            {
+                Category category = context.Categories.Find(id);
+                context.Categories.Remove(category);
                 context.SaveChanges();
             }
         }
--- ClothBazar.Web/Controllers/CategoryController.cs
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -11,16 +11,52 @@ namespace ClothBazar.Web.Controllers
     public class CategoryController : Controller
     {
         CategoriesService categoriesService = new CategoriesService();
+
+        public ActionResult Index()
+        {
+            List<Category> categories = categoriesService.GetCategories();
+            return View(categories);
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Create(Category category)
         {
             categoriesService.SaveCategory(category);
-            return View();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Category category = categoriesService.GetCategory(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoriesService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Category category = categoriesService.GetCategory(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {            
+            categoriesService.DeleteCategory(category.Id);
+            return RedirectToAction("Index");
         }
     }
 }
