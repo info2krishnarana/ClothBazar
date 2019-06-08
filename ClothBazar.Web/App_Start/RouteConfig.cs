adaf34b7658240002b83568a1b107d643af54653
Krishna Rana <info2krishnarana@gmail.com>
2019-06-08 18:19:35 +0530

    custom urls

--- ClothBazar.Web/App_Start/RouteConfig.cs
+++ ClothBazar.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace ClothBazar.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ProductIndex",
+                url: "product/all",
+                defaults: new { controller = "Product", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
